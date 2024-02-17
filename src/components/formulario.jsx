fb91b9108ffe430f72472a4b3a0efed5228400d5
JUDAS89 <lorena.peroza@outlook.com>
2024-02-17 15:59:52 -0300

    logica del formulario

--- src/components/formulario.jsx
+++ src/components/formulario.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import {useState} from 'react'
 
-const formulario = () => {
+const Formulario = () => {
+  const [nombre, setNombre]=useState("")
+  const [email, setEmail]=useState("")
+  const [password, setPassword]=useState("")
+  const [repassword, setRepassword]=useState("")
+  const [error, setError]=useState(false)
+  const [passwordError, setPasswordError]=useState(false)
+
+  const validarRegistro=(e)=>{
+    e.preventDefault()
+
+    if(nombre===''||email===''||password===''||repassword===''){
+      setError(true)
+      return
+    }
+    if(password!==repassword){
+      setError(true)
+      setPasswordError(true)
+      return
+    }
+    setError(false)
+    setPasswordError(false)
+  }
+
   return (
-    <div>formulario</div>
+    <div className='form-group'>
+    <Form onSubmit={validarRegistro}>
+      {error&&<p className="error">{passwordError?"Las contraseñas no coinciden":"No debes dejar campos vacios"}</p>}
+      
+      <Form.Group className="mb-3" controlId="formBasicName">
+        <Form.Control type="text" placeholder="Nombre" value={nombre} onChange={(e)=>setNombre(e.target.value)}/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Control type="email" placeholder="tuemail@ejemplo.com" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Control type="password" placeholder="Contraseña" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicRepassword">
+        <Form.Control type="password" placeholder="Confirma tu contraseña" value={repassword} onChange={(e)=>setRepassword(e.target.value)}/>
+      </Form.Group>
+
+      <Button variant="success" type="submit">
+        Registrarse
+      </Button>
+    </Form>
+
+    </div>
   )
 }
 
-export default formulario
+export default Formulario
--- src/components/registro.jsx
+++ src/components/registro.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Alert } from 'react-bootstrap';
-import formulario from './formulario';
+import Formulario from './formulario';
 import SocialButton from './socialButton';
 import { faFacebookF, faTwitter, faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 const Registro = () => {
-  return (                                                                                                                                                                                                                                                                                                                                                         
-    <div>
-      <h2></h2>
-      <SocialButton icon={faFacebookF} />
-      <SocialButton icon={faTwitter} />
-      <SocialButton icon={faGoogle} />
-      <h4>O usa tu email para registrarte</h4>
-    </div>
-  );
-};
+  return (    
+    <>
+      <div>
+        <h2></h2>
+        <SocialButton icon={faFacebookF} />
+        <SocialButton icon={faTwitter} />
+        <SocialButton icon={faGoogle} />
+        <h4>O usa tu email para registrarte</h4>
+      </div>
+      <Formulario/>
+    </>                                                                                                                                                                                                                                                                                                                                                     
+  )
+}
 
 export default Registro;
